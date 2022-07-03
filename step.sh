023dca0fd4a3413a9866a90208b32c8d3b793e77
rohithmone-lt <monerohith@lambdatest.com>
2022-07-04 00:09:14 +0530

    Removed basicAuth dependency

--- step.sh
+++ step.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -ex
 echo "uploading app ipa to LambdaTest"
-upload_app_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@stage-app-api.lambdatest.com/app/uploadFramework --form type="xcui-ios" --form appFile=@$app_ipa_path)"
+upload_app_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@manual-api.lambdatest.com/app/uploadFramework --form type="xcuit-ios" --form appFile=@$app_ipa_path)"
+
 app_url=$(echo "$upload_app_response" | jq .app_id)
 
 echo "uploading test ipa/zip to LambdaTest"
-upload_test_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@stage-app-api.lambdatest.com/app/uploadFramework --form type="xcui-ios" --form appFile=@$test_ipa_path)"
+upload_test_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@manual-api.lambdatest.com/app/uploadFramework --form type="xcuit-ios" --form appFile=@$test_ipa_path)"
 test_url=$(echo "$upload_test_response" | jq .app_id)
-
-BASICAUTH=$(echo -n $lambdatest_username:$lambdatest_access_key | base64)
 
 echo "starting automated tests"
 json=$( jq -n \
@@ -25,7 +24,7 @@ json=$( jq -n \
                 --arg network_logs "$lambdatest_network_logs" \
                 --arg video "$lambdatest_video" \
                 '{app: $app_url, testSuite: $test_url, device: $devices, build: $build, queueTimeout: $queue_timeout | tonumber, idleTimeout: $idle_timeout | tonumber, geoLocation: $geo_location, tunnel: $tunnel | test("true"), tunnelIdentifier: $tunnel_identifier, devicelog: $device_logs | test("true"), network: $network_logs | test("true"), video: $video | test("true")}')
-run_test_response="$(curl --location --request POST https://qa-beta-api.lambdatest.com/framework/v1/build --header "Authorization: Basic ${BASICAUTH}" --header "Content-Type: application/json" --data-raw \ "$json")"
+run_test_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@mobile-api.lambdatest.com/framework/v1/xcui/build --header "Content-Type: application/json" --data-raw \ "$json")"
 build_id=$(echo "$run_test_response" | jq .buildId)
 
 echo "build id: ${build_id}"
